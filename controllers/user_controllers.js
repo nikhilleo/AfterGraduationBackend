f13f13819e1612939947db02cf737fd8f132fd4e
nikhilleo <ramrakhyani94@gmail.com>
2021-02-14 00:23:25 +0530

    Sending OTP and saving in DB

--- controllers/user_controllers.js
+++ controllers/user_controllers.js
@@ -1,44 +1,61 @@
-
 const User = require("../models/user")
 const OTP = require("../models/otp");
 const validate = require("validator")
 const jwt = require("jsonwebtoken");
 const mail = require("nodemailer");
 
+var OTP_CREATED;
 
-async function sendEmail(email){
+async function sendEmail(email) {
     const transporter = mail.createTransport({
-        service:"hotmail",
-        host:"smtp.office365.com",
-        auth:{
-            user:"aftergraduationck@outlook.com",
-            pass:"nikhil1008"
+        service: "hotmail",
+        host: "smtp.office365.com",
+        auth: {
+            user: "aftergraduationck@outlook.com",
+            pass: "nikhil1008"
         }
     });
-    const otp = Math.floor(Math.random(0.5) * 1000000);
+    var otp = Math.floor(Math.random(0.5) * 1000000);
+    OTP_CREATED = otp;
     const mailOptions = {
-        from:"aftergraduationck@outlook.com",
-        to:email,
-        subject:"Registered",
-        text:`You Have Successfully Registered For Verification Enter OTP ${otp}`
+        from: "aftergraduationck@outlook.com",
+        to: email,
+        subject: "Registered",
+        text: `You Have Successfully Registered For Verification Enter OTP ${otp}`
     }
-    transporter.sendMail(mailOptions,async(err,info)=>{
+    await transporter.sendMail(mailOptions,async(err,info)=>{
         if(err)
         {
             console.log(err)
             // await OTP.findOneAndDelete({otp:Otp._id});
             return;
         }
-        else
+        else if(info.response)
         {
             console.log("Sent " + info.response);
             // console.log(Otp);
-            return otp;
+            return;
         }
     })
+    // const mail = await transporter.sendMail(mailOptions,async(res,err)=>{
+    //     console.log(res);
+    //     if(err)
+    //     {
+    //         console.log(err);
+    //     }
+    // })
+    // .then((res) => {
+    //     console.log("Sent " + res.response);
+    //     // console.log(Otp);
+    //     return otp;
+    // }).catch((err) => {
+    //     console.log(err)
+    //     return;
+    // })
+    // console.log(mail);
 }
 
-exports.signup = async(req, res) => {
+exports.signup = async (req, res) => {
     try {
         console.log(req.body)
         const pass = req.body.password;
@@ -46,7 +63,7 @@ exports.signup = async(req, res) => {
         const phone = req.body.mobile;
         const phone1 = req.body.altmobile;
 
-        
+
         if (email) {
             if (!validate.isEmail(email)) {
                 throw new Error("Invalid Email");
@@ -69,14 +86,17 @@ exports.signup = async(req, res) => {
         const gentoken = await newUser.genAuthToken();
         console.log("gentoken", gentoken);
         await newUser.save();
-        const Otp_sent = sendEmail(req.body.email);
-        console.log(Otp_sent);
-        // const Otp = await new OTP({otp:Otp_sent});
-        // console.log("In DB:",Otp);
+        const otp_sent = await sendEmail(req.body.email);
+        console.log(OTP_CREATED);
+        // const Otp_sent = sendEmail(req.body.email);
+        // console.log(Otp_sent);
+        const Otp = await new OTP({otp:OTP_CREATED});
+        console.log("In DB:",Otp);
+        await Otp.save();
         res.status(201).json({
             message: "User Created",
             user: newUser,
-            // otp:Otp,
+            otp:Otp._id,
             token: gentoken,
         });
     } catch (error) {
@@ -103,20 +123,20 @@ exports.signup = async(req, res) => {
 
 exports.login = async (req, res) => {
     try {
-      let eMail = req.body.email;
-      let passWord = req.body.password;
-      const user = await User.findByCredentials(eMail, passWord);
-      const token = await user.genAuthToken();
-      res.status(200).json({
-        Message: "Login Successfully",
-        token: token,
-        user: user,
-      });
-      console.log(user);
+        let eMail = req.body.email;
+        let passWord = req.body.password;
+        const user = await User.findByCredentials(eMail, passWord);
+        const token = await user.genAuthToken();
+        res.status(200).json({
+            Message: "Login Successfully",
+            token: token,
+            user: user,
+        });
+        console.log(user);
     } catch (error) {
-      if (error.message == "No User Found") {
-        res.status(404).send(error.message + " With Given Credentials");
-      }
+        if (error.message == "No User Found") {
+            res.status(404).send(error.message + " With Given Credentials");
+        }
     }
 };
 
@@ -129,27 +149,21 @@ exports.auth = async (req, res) => {
         res.status(400).send(error.message)
     }
 };
-  
 
 
-exports.getAllUsers = async(req,res)=>{
+
+exports.getAllUsers = async (req, res) => {
     try {
         const users = await User.find({});
-        if(users.length<1)
-        {
+        if (users.length < 1) {
             throw new Error("No Users Found");
-        }
-        else
-        {
+        } else {
             res.status(200).send(users);
         }
     } catch (error) {
-        if(error.message=="No User Found")
-        {
+        if (error.message == "No User Found") {
             res.status(404).send("No Users Registered Yet")
-        }
-        else
-        {
+        } else {
             res.status(400).send(error.message);
         }
     }
