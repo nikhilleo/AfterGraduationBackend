f75ae0211c46bb53256b711808d268fa7b18976e
Nikhil Ramrakhyani <47879184+nikhilleo@users.noreply.github.com>
2021-03-17 00:03:23 +0530

    Updated Link To Call Automatically

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const job_routes = require("./routes/job_routes");
 var cron = require('node-cron');
 
 cron.schedule("0 20,40,0 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 * * *", () => {
-    Axios.get(`https://netflix-clone-backend-1008.herokuapp.com/`).then((res) => {
+    Axios.get(`https://after-graduation-ck.herokuapp.com/`).then((res) => {
         console.log(res.data)
         console.log(Date(Date.now().toLocaleString()))
     }).catch((err) => {
