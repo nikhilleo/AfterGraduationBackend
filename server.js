1c7d6f2855888b4c71c0f24cde6ca9ef068e7579
Nikhil Ramrakhyani <47879184+nikhilleo@users.noreply.github.com>
2021-03-16 23:57:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const bodyParser = require("body-parser")
 const cors = require("cors");
 const user_routes = require("./routes/user_routes");
 const job_routes = require("./routes/job_routes");
+var cron = require('node-cron');
+
+cron.schedule("0 20,40,0 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 * * *", () => {
+    Axios.get(`https://netflix-clone-backend-1008.herokuapp.com/`).then((res) => {
+        console.log(res.data)
+        console.log(Date(Date.now().toLocaleString()))
+    }).catch((err) => {
+        console.log(err.response.data);
+    })
+})
 
 app.use(cors());
 
